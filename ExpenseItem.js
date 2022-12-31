aee02f710d5939f945a16de1c7f29ffcf87a6c4f
robowood <118734805+robowood@users.noreply.github.com>
2022-12-31 15:32:29 +0530

    Add files via upload

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,34 +1,5 @@
-function ExpenseItem(){
-    return (
-        <div>
-         <h2> Expense Item</h2>
-         <p>Food Rs 10</p>
-         <p>Petrol Rs 100</p>
-         <p>Movies Rs 200</p>
-        </div>
-         
-    );
-}
-export default ExpenseItem;
-
-//
 import './ExpenseItem.css';
-function ExpenseItem(){
-    return (
-        <div className="expense-item">
-            <div> March 28th 2021 </div>
-           <div className="expense-item__description"> 
-            <h2> Car Insurance</h2>
-         </div>
-            <div className="expense-item__price"> $294.64</div>
-        </div>
-         
-    );
-}
-export default ExpenseItem;
-// 3
-import './ExpenseItem.css';
-function ExpenseItem(){
+function ExpenseItem(props){
     const expenseDate =new Date(2021,2,28);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 294.64;
@@ -36,14 +7,14 @@ function ExpenseItem(){
 
     return (
         <div className="expense-item">
-            <div> {expenseDate.toISOString()} </div>
+            <div> {props.date.toISOString()} </div>
            <div className="expense-item__description"> 
-            <h2> {expenseTitle }</h2>
+            <h2> {props.title }</h2>
             </div>
-             <div className="expense-item__price"> ${expenseAmount}
+             <div className="expense-item__price"> ${props.amount}
             </div>
             <div className="expense-item__price">
-              <h2> {LocationOfExpenditure}</h2>
+              <h2> {props.location}</h2>
             </div>
 
         </div>
