fea84851fe7ab6b960fe661398a31a5de4ca91e0
robowood <118734805+robowood@users.noreply.github.com>
2022-12-31 14:30:27 +0530

    Update ExpenseItem.js

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -10,3 +10,19 @@ function ExpenseItem(){
     );
 }
 export default ExpenseItem;
+
+//
+import './ExpenseItem.css';
+function ExpenseItem(){
+    return (
+        <div className="expense-item">
+            <div> March 28th 2021 </div>
+           <div className="expense-item__description"> 
+            <h2> Car Insurance</h2>
+         </div>
+            <div className="expense-item__price"> $294.64</div>
+        </div>
+         
+    );
+}
+export default ExpenseItem;
