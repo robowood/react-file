14049e6929ab88974cbe5137d567392f86633698
robowood <118734805+robowood@users.noreply.github.com>
2022-12-31 14:53:57 +0530

    Update ExpenseItem.js

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -26,3 +26,28 @@ function ExpenseItem(){
     );
 }
 export default ExpenseItem;
+// 3
+import './ExpenseItem.css';
+function ExpenseItem(){
+    const expenseDate =new Date(2021,2,28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.64;
+    const LocationOfExpenditure='food';
+
+    return (
+        <div className="expense-item">
+            <div> {expenseDate.toISOString()} </div>
+           <div className="expense-item__description"> 
+            <h2> {expenseTitle }</h2>
+            </div>
+             <div className="expense-item__price"> ${expenseAmount}
+            </div>
+            <div className="expense-item__price">
+              <h2> {LocationOfExpenditure}</h2>
+            </div>
+
+        </div>
+         
+    );
+}
+export default ExpenseItem;
