49afd53b4d1afb7703df50bcb4e3e5721ff5158a
robowood <118734805+robowood@users.noreply.github.com>
2023-01-03 16:54:34 +0530

    Add files via upload

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,10 +1,11 @@
 import React,{useState} from 'react';
 import './ExpenseForm.css';
-function ExpenseForm(){
+function ExpenseForm(props){
 
     const[enteredTitle,setEnteredTitle]=useState('');
     const[enteredAmount,setEnteredAmount]=useState('');
     const[enteredDate,setEnteredDate]=useState('');
+    const[enteredLocation,setEnteredLocation]=useState('');
 
 
     const titleChangeHandeler=(event)=>{
@@ -15,16 +16,40 @@ function ExpenseForm(){
           
     const amountChangeHandler=event=>{
         setEnteredAmount(event.target.value)
-        console.log(event.target.value);
+        console.log("title");
 
     };
-    const deleteChangeHndler=event=>{
+    const dateChangeHndler=event=>{
     setEnteredDate(event.target.value);
+    };
+
+    const locationChangeHndler=event=>{
+        setEnteredLocation(event.target.value);
+        };
+    
+    const submitHandeler=(event)=>{
+        event.preventDefault();
+
+        const expenseData={
+       title:enteredTitle,
+       amount:enteredAmount,
+       date: new Date(enteredDate) ,
+       location:enteredLocation
+
+        };
+        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+        setEnteredLocation('');
 
     };
+
+    
     return(
         <div className="container">
-        <form className="'new-expense__controls">
+        <form className="'new-expense__controls" onSubmit={submitHandeler}>
             <div className='new-expense__control'>
                 <label> Title</label>
                 <input type='text' onChange={titleChangeHandeler}/>
@@ -33,9 +58,14 @@ function ExpenseForm(){
                 <label> Amount</label>
                 <input type='number' min="0.01" step="0.01"onChange={amountChangeHandler}/>
             </div>
+            <div className='new-expense__control'>
+                <label> location</label>
+                <input type='text' min="0.01" step="0.01"onChange={locationChangeHndler}/>
+            </div>
+
             <div className="new-expense__control">
                 <label> Date</label>
-                <input type='date' min="2019-01-01" max="2022-02-31" onchange={deleteChangeHndler}/>
+                <input type='date'  onchange={dateChangeHndler}/>
             </div>
             <div className="new-expense__actions">
                <h2><button type="submit" className="contain1"> Add Expense</button></h2>
