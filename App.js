9a9ba0e10431a7a3a533cf10cf00744a906ebf8e
robowood <118734805+robowood@users.noreply.github.com>
2023-01-03 17:34:52 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,72 +1,63 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
+import ExpenseForm from './Components/Expenses/ExpenseForm';
 import './App.css';
 
-import ExpenseItem from './Components/ExpenseItem';
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      LocationOfExpenditure: 'food'
-    },
-    { id: 'e2', 
-        title: 'New TV',
-      amount: 799.49, 
-      date: new Date(2021, 2, 12),
-      LocationOfExpenditure: 'petrol'
-     },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      LocationOfExpenditure: 'grocery'
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      LocationOfExpenditure:'movie '
-    },
-  ];
+import Expenses from './Components/Expenses/Expenses';
+const DUMMY_EXPENSE = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: 'food'
+  },
+  { id: 'e2', 
+      title: 'New TV',
+    amount: 799.49, 
+    date: new Date(2021, 2, 12),
+    location: 'petrol'
+   },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location: 'grocery'
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location:'movie '
+  },
+];
+
+const App=() =>{
+
+        const [expenses,setExpensesData]=useState(DUMMY_EXPENSE);
+     
+       const saveExpenseDataHandeler=(enteredExpenseData)=>{
+           const expense={
+              ...enteredExpenseData,
+              id:Math.random().toString()
+           }
+           const updatedExpense=[expense,...expenses]
+           setExpensesData(updatedExpense);
+           console.log(updatedExpense);
+
+       };
+     
+
   return (
     <div>
-
-      <h2> Let's get started....</h2>
+         
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandeler}></ExpenseForm>
+      <Expenses expenses={expenses}/>
       
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date={expenses[0].date}
-      location={expenses[0].LocationOfExpenditure}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title} 
-      amount={expenses[1].amount} 
-      date={expenses[1].date}
-      location={expenses[1].LocationOfExpenditure}
+      
 
-      ></ExpenseItem>
-       <ExpenseItem
-      title={expenses[2].title} 
-      amount={expenses[2].amount} 
-      date={expenses[2].date}
-      location={expenses[2].LocationOfExpenditure}
-
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[3].title} 
-      amount={expenses[3].amount} 
-      date={expenses[3].date}
-      location={expenses[3].LocationOfExpenditure}
-
-      ></ExpenseItem>
-            
-
-
+     
 
 
       
