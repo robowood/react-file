7552d5fff1a7c85b4f1e90e556019b0dde54b53a
robowood <118734805+robowood@users.noreply.github.com>
2023-01-03 12:54:59 +0530

    Add files via upload

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,23 +1,47 @@
+import React,{useState} from 'react';
 import './ExpenseForm.css';
 function ExpenseForm(){
+
+    const[enteredTitle,setEnteredTitle]=useState('');
+    const[enteredAmount,setEnteredAmount]=useState('');
+    const[enteredDate,setEnteredDate]=useState('');
+
+
+    const titleChangeHandeler=(event)=>{
+    setEnteredTitle(event.target.value);
+
+        console.log('title changed');
+    };
+          
+    const amountChangeHandler=event=>{
+        setEnteredAmount(event.target.value)
+        console.log(event.target.value);
+
+    };
+    const deleteChangeHndler=event=>{
+    setEnteredDate(event.target.value);
+
+    };
     return(
+        <div className="container">
         <form className="'new-expense__controls">
             <div className='new-expense__control'>
                 <label> Title</label>
-                <input type='text'/>
+                <input type='text' onChange={titleChangeHandeler}/>
             </div>
             <div className='new-expense__control'>
                 <label> Amount</label>
-                <input type='number' min="0.01" step="0.01"/>
+                <input type='number' min="0.01" step="0.01"onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label> Date</label>
-                <input type='date' min="2019-01-01" max="2022-02-31"/>
+                <input type='date' min="2019-01-01" max="2022-02-31" onchange={deleteChangeHndler}/>
             </div>
             <div className="new-expense__actions">
-                <button type="submit"> Add Expense</button>
+               <h2><button type="submit" className="contain1"> Add Expense</button></h2>
             </div>
         </form>
+        </div>
        
 
     );
