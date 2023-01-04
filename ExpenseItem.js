db69fd682510bb8e2a5d219f0a9c2a4fe9a46cdd
robowood <118734805+robowood@users.noreply.github.com>
2023-01-04 22:20:41 +0530

    Add files via upload

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -9,18 +9,29 @@ import ExpenseDetails from './ExpenseDetails';
 const ExpenseItem=(props)=>{
 
     const [amount,setAmount]=useState(props.amount);
-    
-    function DeleteExpense(){
-        setAmount(100);
+     const [title,setTitle]=useState(props.title);
+    const [date,setDate]=useState(props.date);
+     const [location,setLocation]=useState(props.location);
+
+
+
+    function updateExpense(){
+        setAmount('');
+        setTitle('');
+         setDate('');
+         setLocation('');
+
         
     }
 
     return (
+        <div className="contain">
         <div className="expense-item">
-         <ExpenseDate date={props.date}></ExpenseDate>
-         <ExpenseDetails title={props.title} amount={amount} location={props.location}></ExpenseDetails>
-         <button onClick={DeleteExpense}> Delete Expense</button>
+         <ExpenseDate date={date}></ExpenseDate>
+         <ExpenseDetails title={title} amount={amount} location={location}></ExpenseDetails>
+         <button onClick={updateExpense}> Delete Expense</button>
         </div> 
+        </div>
 
 
   
